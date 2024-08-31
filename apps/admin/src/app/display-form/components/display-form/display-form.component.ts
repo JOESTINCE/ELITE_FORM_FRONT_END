b59735d74b0f996903201436bdd4dfa0fd38d0e3
Jestus <joestincejestus@gmail.com>
2024-08-31 16:37:46 +0530

    changes in display forms

--- apps/admin/src/app/display-form/components/display-form/display-form.component.ts
+++ apps/admin/src/app/display-form/components/display-form/display-form.component.ts
@@ -59,8 +59,11 @@ export class DisplayFormComponent {
     ]
   }; 
   form: any;
+  isFormInitialized: boolean=false;
   ngOnInit(){
+    console.log('ngOnInit is called')
     this.initializeForm();
+    console.log('formDetails',this.form.value)
   }
   ngAfterViewInit(){
     const overlaypane: any = document.querySelectorAll('.mdc-form-field');
@@ -69,17 +72,57 @@ export class DisplayFormComponent {
         item.style['font-size'] = '17px';
       })
     }
-
   }
+  // ngAfterContentInit (){
+  //   this.isFormInitialized = true
+  // } 
   initializeForm(){
-    this.form = new FormArray([]);
+    console.log('inside initialize form')
+    this.form = new FormGroup({items : new FormArray([])});
     if(this.formData?.items?.length){
      for(let i=0; i<this.formData.items.length; i++){
-       this.form.push(new FormGroup({
-         userAnswer: new FormControl(null)
+       let items = this.form.get('items') as FormArray
+        items.push(new FormGroup({
+         question: new FormControl(this.formData?.items?.[i]?.question),
+         answerType: new FormControl(this.formData?.items?.[i]?.answerType),
+         userAnswer: new FormControl(null),
+         options: new FormArray([])
        }))
+       if(this.formData?.items?.[i]?.answer?.length){
+         for (let j = 0; j < this.formData.items[i].answer.length; j++){
+           let optionsArray = ((this.form.get('items') as FormArray).at(i).get('options') as FormArray);
+           if(this.formData.items[i].answerType === 'checkBox'){
+             optionsArray.push(new FormGroup({
+               option: new FormControl(false),
+               value: new FormControl(this.formData.items[i].answer[j].answerDetails)
+             })); 
+           }
+           else{
+             optionsArray.push(new FormGroup({
+               option: new FormControl(this.formData.items[i].answer[j].answerDetails)
+             }));
+
+           }
+         }
+       }
      }
     }
-
+  }
+  onFormSubmit(){
+    console.log(this.form.value)
+  }
+  ohCheckBoxChange(event: any, outerIndex: any, innerIndex: any){
+    let userAnswer = ((this.form.get('items') as FormArray).at(outerIndex) as FormGroup).get('userAnswer')?.value ?? [];
+    if (event && outerIndex >=0 && innerIndex >=0){
+      if(this.formData?.items?.[outerIndex]?.answer?.[innerIndex]?.answerDetails){
+        userAnswer.push(this.formData.items[outerIndex].answer[innerIndex].answerDetails)
+      }
+    }
+    else if (outerIndex >= 0 && innerIndex >= 0){
+      if (this.formData?.items?.[outerIndex]?.answer?.[innerIndex]?.answerDetails){
+        userAnswer.splice(userAnswer.indexOf(this.formData.items[outerIndex].answer[innerIndex].answerDetails), 1);
+      }
+    }
+      ((this.form.get('items') as FormArray).at(outerIndex) as FormGroup).get('userAnswer')?.setValue(userAnswer);
   }
 }
